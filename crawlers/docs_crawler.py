a9336b8ba294086193bd28ec8c99e0f6c562542e
Ofer Mendelevitch <ofer@vectara.com>
2023-08-26 15:29:56 -0700

    bug fix in docs_crawler

--- crawlers/docs_crawler.py
+++ crawlers/docs_crawler.py
@@ -34,7 +34,7 @@ class DocsCrawler(Crawler):
         meta_refresh = soup.find('meta', attrs={'http-equiv': 'refresh'})
         if meta_refresh:
             href = meta_refresh['content'].split('url=')[-1]
-            url = self.parse_url_and_href(url, href)
+            url = self.concat_url_and_href(url, href)
             response = self.session.get(url, headers=headers)
             if response.status_code != 200:
                 logging.info(f"Failed to crawl redirect {url}, response code is {response.status_code}")
