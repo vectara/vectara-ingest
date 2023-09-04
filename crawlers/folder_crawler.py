ff68e20cf1e14c784cc889b9633e877c73f8ee17
Ofer Mendelevitch <ofer@vectara.com>
2023-09-04 13:38:16 -0700

    update to folder crawler

--- crawlers/folder_crawler.py
+++ crawlers/folder_crawler.py
@@ -12,6 +12,7 @@ class FolderCrawler(Crawler):
 
         # Walk the directory and upload files with the specified extension to Vectara
         logging.info(f"indexing files in {self.cfg.folder_crawler.path} with extensions {extensions}")
+        source = self.cfg.folder_crawler.source
         for root, _, files in os.walk(folder):
             for file in files:
                 file_extension = pathlib.Path(file).suffix
@@ -22,8 +23,8 @@ class FolderCrawler(Crawler):
                         'created_at': time.strftime('%Y-%m-%dT%H:%M:%S', time.gmtime(os.path.getctime(file_path))),
                         'modified_at': time.strftime('%Y-%m-%dT%H:%M:%S', time.gmtime(os.path.getmtime(file_path))),
                         'file_size': os.path.getsize(file_path),
-                        'source': 'folder',
+                        'source': source,
                         'title': file_name,
-                        'url': file_name
+#                        'url': file_name
                     }
                     self.indexer.index_file(filename=file_path, uri=file_name, metadata=file_metadata)
