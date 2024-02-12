6f19f2f411daf61d48ea2a5ef34287c0372aea40
Ofer Mendelevitch <ofermend@gmail.com>
2024-02-11 20:29:28 -0800

    bugfix

--- core/indexer.py
+++ core/indexer.py
@@ -288,9 +288,12 @@ class Indexer(object):
                     logging.info(f"Failed to crawl {url} to get PDF content with error {e}, skipping...")
                     return False
 
+        else:
             # If MD, RST of IPYNB file, then we don't need playwright - can just download content directly and convert to text
-            elif url.endswith(".md") or url.endswith(".rst") or url.lower().endswith(".ipynb"):
-                dl_content = content.decode('utf-8')
+            if url.endswith(".md") or url.endswith(".rst") or url.lower().endswith(".ipynb"):
+                response = self.session.get(url, timeout=self.timeout)
+                response.raise_for_status()
+                dl_content = response.content.decode('utf-8')
                 if url.endswith('rst'):
                     html_content = docutils.core.publish_string(dl_content, writer_name='html')
                 elif url.endswith('md'):
