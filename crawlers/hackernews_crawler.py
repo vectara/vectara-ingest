69a2bbd45a604b3ad0ef7214660cba4cde1cbcf8
Ofer Mendelevitch <ofermend@gmail.com>
2024-03-19 07:41:17 -0700

    updated hackernews crawler

--- crawlers/hackernews_crawler.py
+++ crawlers/hackernews_crawler.py
@@ -1,44 +1,68 @@
 
-import requests
+import json
+from omegaconf import OmegaConf
 import logging
 from core.crawler import Crawler
-import os
 from core.utils import html_to_text, create_session_with_retries
-from slugify import slugify
-
+import datetime
 from typing import List
-
-def get_comments(kids: List[str], entrypoint: str) -> List[str]:
-    comments = []
-    for kid in kids:
-        try:
-            response = requests.get(entrypoint + 'item/{}.json'.format(kid))
-            comment = response.json()
-        except Exception as e:
-            logging.info(f"Error retrieving comment {kid}, e={e}")
-            comment = None
-        if comment is not None and comment.get('type', '') == 'comment':
-            comments.append(html_to_text(comment.get('text', '')))
-            sub_kids = comment.get('kids', [])
-            if len(sub_kids)>0:
-                comments += get_comments(sub_kids, entrypoint)
-    return comments
 
 class HackernewsCrawler(Crawler):
 
+    def __init__(self, cfg: OmegaConf, endpoint: str, customer_id: str, corpus_id: int, api_key: str) -> None:
+        super().__init__(cfg, endpoint, customer_id, corpus_id, api_key)
+        self.N_ARTICLES = self.cfg.hackernews_crawler.max_articles
+        self.days_back = self.cfg.hackernews_crawler.get("days_back", 3)
+        self.indexer.reindex = True     # always reindex with hackernews, since it depends on comments
+        self.db_url = 'https://hacker-news.firebaseio.com/v0/'
+        self.session = create_session_with_retries()
+
+    def get_comments(self, story: dict) -> List[str]:
+        comments = []
+        kids = [str(k) for k in story.get('kids', [])]
+        for kid in kids:
+            try:
+                response = self.session.get(self.db_url + 'item/{}.json'.format(kid))
+                comment = response.json()
+            except Exception as e:
+                logging.info(f"Error retrieving comment {kid}, e={e}")
+                comment = None
+            if comment is not None and comment.get('type', '') == 'comment':
+                comments.append(comment)
+                comments += self.get_comments(comment)
+        return comments
+
+    def index_story(self, id: str) -> None:
+        url = f'https://news.ycombinator.com/item?id={id}'
+        story = self.session.get(self.db_url + 'item/{}.json'.format(id)).json()
+        doc_id = str(story['id'])
+        doc_title = html_to_text(story.get('title', ''))
+        doc_text = html_to_text(story.get('text', ''))
+        doc_metadata = {'source': 'hackernews', 'title': doc_title, 'url': url}
+        texts = [] if len(doc_text) == 0 else [doc_text]
+        titles = ['']
+        times = [datetime.datetime.fromtimestamp(story.get('time', 0)).date()]
+        comments = self.get_comments(story)
+        for comment in comments:
+            texts.append(html_to_text(comment.get('text', '')))
+            titles.append(html_to_text(comment.get('title', '')))
+            times.append(datetime.datetime.fromtimestamp(comment.get('time', 0)).date())
+        
+        # if most recent comment is older than days_back, don't index
+        if max(times) < datetime.datetime.now().date() - datetime.timedelta(days=self.days_back):
+            logging.info(f"Skipping story {id} because most recent comment is older than {self.days_back} days")
+            return
+            
+        self.indexer.index_segments(doc_id=doc_id, 
+                                    texts=texts, titles=titles, metadatas=None,
+                                    doc_metadata=doc_metadata, doc_title=doc_title)
+
     def crawl(self) -> None:
-        N_ARTICLES = self.cfg.hackernews_crawler.max_articles
-
-        # URL for the Hacker News API
-        entrypoint = 'https://hacker-news.firebaseio.com/v0/'
-
         # Retrieve the IDs of the top N_ARTICLES stories
-        session = create_session_with_retries()
-        resp1= session.get(entrypoint + 'topstories.json')
-        resp2 = session.get(entrypoint + 'newstories.json')
-        resp3 = session.get(entrypoint + 'beststories.json')
-
-        top_ids = list(set(list(resp1.json()) + list(resp2.json()) + list(resp3.json())))[:N_ARTICLES]
+        resp1= self.session.get(self.db_url + 'topstories.json')
+        resp2 = self.session.get(self.db_url + 'newstories.json')
+        resp3 = self.session.get(self.db_url + 'beststories.json')
+        top_ids = list(set(list(resp1.json()) + list(resp2.json()) + list(resp3.json())))[:self.N_ARTICLES]
         num_ids = len(top_ids)
         logging.info(f"Crawling {num_ids} stories")
 
@@ -47,22 +71,7 @@ class HackernewsCrawler(Crawler):
             if n_id % 20 == 0:
                 logging.info(f"Crawled {n_id} stories so far")
             try:
-                response = session.get(entrypoint + 'item/{}.json'.format(id))
-                story = response.json()
-                url = story.get('url', None)
-                if url is None:
-                    continue
-                title = html_to_text(story.get('title', ''))
-                text = story.get('text', None)
-                if text:
-                    fname = slugify(url) + ".html"
-                    with open(fname, 'w') as f:
-                        f.write(text)
-                    self.indexer.index_file(fname, uri=url, metadata={'title': title, 'url': url})
-                    os.remove(fname)
-                else:
-                    metadata = {'source': 'hackernews', 'title': title, 'url': url}
-                    self.indexer.index_url(url, metadata=metadata)
+                self.index_story(str(id))
             except Exception as e:
                 import traceback
-                logging.error(f"Error crawling story {url}, error={e}, traceback={traceback.format_exc()}")
+                logging.error(f"Error crawling story {id}, error={e}, traceback={traceback.format_exc()}")
