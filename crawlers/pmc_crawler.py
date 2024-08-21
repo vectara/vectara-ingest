27e6c516c2b25cfd692ca87b75a947fc7a3a7216
Ofer Mendelevitch <ofermend@gmail.com>
2024-08-21 07:01:54 -0700

    update

--- crawlers/pmc_crawler.py
+++ crawlers/pmc_crawler.py
@@ -30,15 +30,6 @@ def get_top_n_papers(topic: str, n: int, email: str) -> Any:
     return id_list
 
 
-get_headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
-    "Accept-Language": "en-US,en;q=0.5",
-    "Accept-Encoding": "gzip, deflate",
-    "Connection": "keep-alive",
-}
-
-
 class PmcCrawler(Crawler):
 
     def __init__(self, cfg: OmegaConf, endpoint: str, customer_id: str, corpus_id: int, api_key: str) -> None:
