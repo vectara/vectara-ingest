e07a5f822aacd09ebe8e03a434af230ca25514d2
Jeremy Custenborder <jcustenborder@gmail.com>
2025-02-21 21:41:57 -0600

    Changed the IDs to be based off the file name. Added start_page and end_page to metadata.

--- core/indexer.py
+++ core/indexer.py
@@ -36,7 +36,7 @@ from core.utils import (
 from core.extract import get_article_content
 from core.doc_parser import UnstructuredDocumentParser, DoclingDocumentParser, LlamaParseDocumentParser
 from core.contextual import ContextualChunker
-from PyPDF2 import PdfReader, PdfWriter
+from pypdf import PdfReader, PdfWriter
 import tempfile
 
 # Suppress FutureWarning related to torch.load
@@ -618,7 +618,7 @@ class Indexer:
             self.logger.error(f"REST upload for {uri} failed with code {response.status_code}, text = {response.text}")
             return False
 
-        self.logger.info(f"REST upload for {uri} successful")
+        self.logger.info(f"REST upload for {uri} ({upload_filename}) successful")
         self.store_file(filename, url_to_filename(uri))
         return True
 
@@ -946,8 +946,7 @@ class Indexer:
         filesize_mb = get_file_size_in_MB(filename)
 
         if (any(uri.endswith(extension) for extension in large_file_extensions) and
-            (filesize_mb >= size_limit or self.contextual_chunking or
-             self.summarize_images or self.enable_gmft)
+            (self.contextual_chunking or self.summarize_images or self.enable_gmft)
         ):
             self.process_locally = True
 
@@ -980,28 +979,33 @@ class Indexer:
             else:
                 ex_metadata = {}
             metadata.update(ex_metadata)
+            metadata['file_name'] = filename.split('/')[-1]
 
-            max_pdf_size = 50
-            pages_per_pdf = 50
+            max_pdf_size = int(self.cfg.vectara.get('max_pdf_size', 50))
+            pages_per_pdf = int(self.cfg.vectara.get('pages_per_pdf', 100))
 
             if filesize_mb > max_pdf_size:
                 pdf_reader = PdfReader(filename)
                 total_pages = len(pdf_reader.pages)
-                logging.info(f"{filename} is {filesize_mb} which is larger than {max_pdf_size} mb with #{total_pages} pages. Splitting into #{pages_per_pdf} chunks")
+                logging.info(f"{filename} is {filesize_mb} which is larger than {max_pdf_size} mb with {total_pages} pages. Splitting into {pages_per_pdf} chunks")
                 error_count = 0
                 for i in range(0, total_pages, pages_per_pdf):
                     pdf_writer = PdfWriter()
+                    part_metadata = {
+                        "start_page": i,
+                        "end_page": min(i + pages_per_pdf, total_pages)
+                    }
                     for j in range(i, min(i + pages_per_pdf, total_pages)):
                         pdf_writer.add_page(pdf_reader.pages[j])
-                    part_id = f"#{filename}-#{i}"
-                    part_metadata = {}
+                    part_id = f"{metadata['file_name']}-{i}"
+
                     part_metadata.update(metadata)
                     with tempfile.NamedTemporaryFile(suffix=".pdf", mode='wb', delete=False) as f:
                         pdf_writer.write(f)
                         f.flush()
                         f.close()
                         try:
-                            part_success = self.indexer._index_file(f.name, uri, part_metadata, part_id)
+                            part_success = self._index_file(f.name, uri, part_metadata, part_id)
                             if not part_success:
                                 error_count+=1
                         finally:
